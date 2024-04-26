e6dae538a6832a3b218480d99642fc9bb531fc35
artikosamodra <artikosamudra@gmail.com>
2024-04-26 11:56:33 +0700

    navbar commit

---
+++ src/app/component/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <>
+      <div className="grid grid-cols-10 text-center">
+        <div className="col-span-3">Logo</div>
+        <div className="col-span-3">Search</div>
+        <div className="col-span-3">Login</div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
