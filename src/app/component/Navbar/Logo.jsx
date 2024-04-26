403cb85338119771f4e5c84da4496916cfbb0ab5
artikosamodra <artikosamudra@gmail.com>
2024-04-26 19:06:59 +0700

    commit navbar UI

--- src/app/component/Navbar/Logo.jsx
+++ src/app/component/Navbar/Logo.jsx
@@ -1,3 +1,4 @@
+import { House } from "@phosphor-icons/react/dist/ssr";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -6,7 +7,16 @@ const Logo = () => {
     <>
       <div className="flex justify-center items-center">
         <Link href="/">
-          <Image src="/img/logoini.png" alt="logo" width={100} height={100} />
+          <Image
+            src="/img/logoini.png"
+            alt="logo"
+            width={100}
+            height={100}
+            className="md:flex hidden"
+          />
+          <div className="md:hidden flex">
+            <House size={24} weight="fill" />
+          </div>
         </Link>
       </div>
     </>
--- src/app/component/Navbar/NavBot.jsx
+++ src/app/component/Navbar/NavBot.jsx
@@ -1,11 +1,12 @@
 import { Bell, House, Play, User } from "@phosphor-icons/react/dist/ssr";
+import Logo from "./Logo";
 
 const NavBot = () => {
   return (
     <>
       <div className="grid grid-cols-4 md:hidden bottom-0 fixed w-full md:px-10 bg-orange-600">
         <button className="py-3 hover:bg-stone-200 flex justify-center text-orange-100 hover:text-stone-700">
-          <House size={24} weight="fill" />
+          <Logo />
         </button>
         <button className="py-3 hover:bg-stone-200 flex justify-center text-orange-100 hover:text-stone-700">
           <Play size={24} weight="fill" />
