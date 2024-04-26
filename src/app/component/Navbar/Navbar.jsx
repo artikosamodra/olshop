71412c5be33926aeef5083ddcc5d44aa4c448077
artikosamodra <artikosamudra@gmail.com>
2024-04-26 14:55:09 +0700

    commit structur navbar

--- src/app/component/Navbar/Navbar.jsx
+++ src/app/component/Navbar/Navbar.jsx
@@ -1,10 +1,10 @@
 const Navbar = () => {
   return (
     <>
-      <div className="grid grid-cols-10 text-center">
-        <div className="col-span-3">Logo</div>
-        <div className="col-span-3">Search</div>
-        <div className="col-span-3">Login</div>
+      <div className="grid grid-cols-10 py-5 px-10 bg-rose-300 gap-10 flex text-center">
+        <div className="col-span-2">Logo</div>
+        <div className="col-span-6">Search</div>
+        <div className="col-span-2">Login</div>
       </div>
     </>
   );
