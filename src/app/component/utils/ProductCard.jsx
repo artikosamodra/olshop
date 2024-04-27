b32a480ef58dce21bffed596954d1a92d9f5f65e
artikosamodra <artikosamudra@gmail.com>
2024-04-27 17:53:19 +0700

    setting Product Card Component

--- src/app/component/utils/ProductCard.jsx
+++ src/app/component/utils/ProductCard.jsx
@@ -3,18 +3,26 @@ import Image from "next/image";
 const ProductCard = () => {
   return (
     <>
-      <div className="flex flex-col bg-stone-50 px-2 rounded-md">
+      <div className="flex flex-col bg-stone-50 rounded-md">
         <div>
-          <Image src="/img/logo" alt="product" width={300} height={300} />
+          <Image
+            src="/img/logo"
+            alt="product"
+            width={300}
+            height={300}
+            className="border"
+          />
         </div>
-        <div>Nama Product</div>
-        <div>
-          <div>Price</div>
-          <div>Diskon</div>
-        </div>
-        <div>
-          <div>Rating</div>
-          <div>Terjual</div>
+        <div className="px-2 py-4">
+          <div>Nama Product</div>
+          <div className="flex justify-between">
+            <div>Price</div>
+            <div>Diskon</div>
+          </div>
+          <div className="flex justify-between">
+            <div>Rating</div>
+            <div>Terjual</div>
+          </div>
         </div>
       </div>
     </>
