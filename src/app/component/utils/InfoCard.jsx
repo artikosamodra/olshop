5d7d62dce6e8d5b8e904f9405cc4ea104e1c50d9
artikosamodra <artikosamudra@gmail.com>
2024-05-07 15:04:38 +0700

    add logic -+ at buy stock > detail produk

--- src/app/component/utils/InfoCard.jsx
+++ src/app/component/utils/InfoCard.jsx
@@ -1,12 +1,29 @@
 "use client";
 import { Star } from "@phosphor-icons/react";
+import { useCallback, useEffect, useState } from "react";
 
 const InfoCard = ({ nameP, catP, priceP }) => {
+  const [add, setAdd] = useState(1);
+
+  const increment = useCallback(() => {
+    setAdd((prev) => prev + 1);
+  });
+
+  const decrement = useCallback(() => {
+    setAdd((prev) => prev - 1);
+  });
+
+  useEffect(() => {
+    if (add < 0) {
+      setAdd(0);
+    }
+  }, [add]);
+
   return (
     <>
       <div className="bg-stone-100 px-10 py-4">
-        <div className="px-2 text-2xl font-bold pb-2">{nameP}</div>
-        <div className="flex gap-10 px-2">
+        <div className="px-2 text-2xl font-bold pb-4">{nameP}</div>
+        <div className="flex gap-10 px-2 pb-2">
           <div className="text-yellow-400 flex">
             <Star size={20} weight="fill" />
             <Star size={20} weight="fill" />
@@ -16,7 +33,7 @@ const InfoCard = ({ nameP, catP, priceP }) => {
           </div>
           <div>900 Terjual</div>
         </div>
-        <div>
+        <div className="">
           <table>
             <tr>
               <td>Kategori</td>
@@ -33,12 +50,19 @@ const InfoCard = ({ nameP, catP, priceP }) => {
           </table>
         </div>
         <div className="flex gap-5 px-2 pt-5 text-2xl">
-          <div className="text-orange-500 text-xl font-bold">$ {priceP}</div>
+          <div className="text-orange-500 text-2xl border border-stone-700 p-1 font-bold">
+            $ {priceP}
+          </div>
           {/* <div className="text-orange-500 text-xl font-bold">Rp. 76.000,-</div> */}
         </div>
         <div className="flex gap-2 items-center pt-5 px-2">
-          <button className="px-2 bg-stone-400 shadow-md">-</button>Number
-          <button className="px-2 bg-stone-400 shadow-md">+</button>
+          <button className="px-2 bg-stone-400 shadow-md" onClick={decrement}>
+            -
+          </button>
+          {add}
+          <button className="px-2 bg-stone-400 shadow-md" onClick={increment}>
+            +
+          </button>
         </div>
         <div className="flex gap-2 px-2 pt-10">
           <button className="px-10 py-2 bg-red-600 shadow-md text-white font-semibold rounded-md">
--- src/app/pages/detail/[id]/page.jsx
+++ src/app/pages/detail/[id]/page.jsx
@@ -2,53 +2,17 @@
 import InfoCard from "@/app/component/utils/InfoCard";
 import Loading from "@/app/loading";
 import Image from "next/image";
-import { useEffect, useState } from "react";
 
 const Detail = async ({ params: { id } }) => {
-  if (!id) {
-    return;
-  }
-
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_BASE_URL}/products/${id}`
   );
   const detail = await response.json();
 
-  // const [detail, setDetail] = useState(null);
-
-  // // const ProductsFetch = async ({ params: { id } }) => {
-  // //   const response = await fetch(
-  // //     `${process.env.NEXT_PUBLIC_API_BASE_URL}/products/${id}`
-  // //   );
-  // //   const respJson = await response.json();
-  // //   setDetail(respJson);
-  // // };
-
-  // const ProductsFetch = async () => {
-  //   // if (!id) return;
-  //   const response = await fetch(
-  //     `${process.env.NEXT_PUBLIC_API_BASE_URL}/products/${id}`
-  //   );
-  //   const respJson = await response.json();
-  //   // console.log(respJson);
-  //   setDetail(respJson);
-  // };
-  // useEffect(() => {
-  //   ProductsFetch();
-  // }, []);
-
-  if (!detail) {
-    return (
-      <div className="flex justify-center items-center col-span-6">
-        <Loading />
-      </div>
-    );
-  }
-
   return (
     <>
       <section className="pt-40 px-20">
-        <div className="grid grid-cols-10]">
+        <div className="grid grid-cols-10">
           <div className="col-span-3" key={id}>
             <Image
               src={detail.image}
